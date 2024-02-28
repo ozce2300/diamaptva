4a9ebe9f0c955fe2270516b314d07cd239c8f5a2
Özgur Celik <ozce2300@student.miun.se>
2024-02-28 18:05:54 +0100

    Diamap2

---
+++ dist/index.66f57edb.js
@@ -0,0 +1,2 @@
+let e=document.getElementById("open-menu"),t=document.getElementById("close-menu");function n(){let e=document.getElementById("nav-menu");"none"===window.getComputedStyle(e).display?e.style.display="block":e.style.display="none"}e.addEventListener("click",n),t.addEventListener("click",n),setInterval(function(){let e=new Date;document.getElementById("klockan").innerHTML=e.toLocaleTimeString()},100),document.getElementById("btn"),document.getElementById("boll"),btn.addEventListener("click",function(){boll.classList.add("hoppa")});
+//# sourceMappingURL=index.66f57edb.js.map
